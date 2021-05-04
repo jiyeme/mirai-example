9407d2fadd741f17eae0d5709e36ffb0e39c754d
jiye <jiyecafe@gmail.com>
2021-05-04 11:02:12 +0800

    refactor: 构建工具由maven换至gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "org.example"
+version = "1.0"
+
+repositories {
+    jcenter()
+    maven(url = "https://dl.bintray.com/karlatemp/misc")
+    mavenCentral()
+}
+
+tasks.withType(KotlinJvmCompile::class.java) {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    val miraiVersion = "2.6.2"
+    api("net.mamoe", "mirai-core-api", miraiVersion)     // 编译代码使用
+    runtimeOnly("net.mamoe", "mirai-core", miraiVersion) // 运行时使用
+
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
